c40906d4712ee7c1271dec0f00b8c6e8e3fc6c1a
Michael Morowitz <github@worldpost.us>
2023-02-19 15:26:16 -0600

    first commit

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	"title": "Eleventy Base Blog v8",
-	"url": "https://example.com/",
+	"title": "morowitz.com",
+	"url": "https://morowitz.com/",
 	"language": "en",
-	"description": "I am writing about my experiences as a naval navel-gazer.",
+	"description": "",
 	"author": {
-		"name": "Your Name Here",
-		"email": "youremailaddress@example.com",
-		"url": "https://example.com/about-me/"
+		"name": "Michael Morowitz",
+		"email": "michael@morowitz.com",
+		"url": "https://morowitz.com/about-me"
 	}
 }
